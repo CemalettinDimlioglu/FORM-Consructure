bc74d05653bcb18b042b4a6f21e95204c021df4f
CMLTTN <cemaldim@gmail.com>
2023-06-02 22:29:57 +0300

    all

--- rc06_forms-otherEvents/src/App.js
+++ rc06_forms-otherEvents/src/App.js
@@ -1,9 +1,14 @@
+// import Form from "./components/1-forms/Form"
+
+import FormWithObject from "./components/1-forms/FormWithObject"
+
  
 
 function App() {
   return (
     <div >
-      
+      {/* <Form/> */}
+      <FormWithObject/>
     </div>
   )
 }
--- rc06_forms-otherEvents/src/components/1-forms/Form.jsx
+++ rc06_forms-otherEvents/src/components/1-forms/Form.jsx
@@ -1,1 +1,32 @@
+ const Form = () =>{
+
+  return(
  
+ <form>
+   <h1 className="text-danger display-6 text-center">FORMS</h1>
+   <div className="mb-3">
+     <label htmlFor="usurname" className="form-label">
+       User Name
+     </label>
+     <input type="text" className="form-control" id="username" />
+   </div>
+   <div className="mb-3">
+     <label htmlFor="email" className="form-label">
+       Email
+     </label>
+     <input type="email" className="form-control" id="email" />
+   </div>
+   <div className="mb-3">
+     <label htmlFor="password" className="form-label">
+       Password
+     </label>
+     <input type="password" className="form-control" id="password" />
+   </div>
+   <button type="submit" className="btn btn-danger">
+     Submit
+   </button>
+ </form>
+
+ )}
+
+ export default Form
