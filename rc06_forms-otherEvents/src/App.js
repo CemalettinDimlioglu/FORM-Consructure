94a3698b00559cf56c530516f8f24376721e27bd
CMLTTN <cemaldim@gmail.com>
2023-06-03 12:13:38 +0300

    events

--- rc06_forms-otherEvents/src/App.js
+++ rc06_forms-otherEvents/src/App.js
@@ -1,14 +1,16 @@
-import Form from "./components/1-forms/Form"
+// import Form from "./components/1-forms/Form"
 
 // import FormWithObject from "./components/1-forms/FormWithObject"
+import KeyboardClipboard from "./components/2-keyboardClipboardEvents/KeyboardClipboard"
 
  
 
 function App() {
   return (
-    <div >
-      <Form/>
+    <div className="container mt-4 text center" >
+      {/* <Form/> */}
       {/* <FormWithObject/> */}
+      <KeyboardClipboard/>
     </div>
   )
 }
